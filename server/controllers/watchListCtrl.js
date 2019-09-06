609bc7c3cc0827ad9253db63e368869d72d30adb
Brian Lowe <mynameisboy@gmail.com>
2019-09-06 10:59:29 -0600

    finished backend endpoints...i think

--- server/controllers/watchListCtrl.js
+++ server/controllers/watchListCtrl.js
@@ -4,10 +4,29 @@ const {watchList} = require("../data/watchListData");
 module.exports = {
   addToWatchList: (req, res) => {
     const runObj = req.body;
+    runObj.watched = false
     watchList.push(runObj);
     res.status(200).send(watchList);
   },
   getWatchList: (req, res) => {
     res.status(200).send(watchList);
+  },
+  updateWatchList: (req, res) => {
+    const {id} = req.params
+    for (let i = 0; i < watchList.length; i++) {
+      if (watchList[i].id === id) {
+        watchList[i].watched = !watchList[i].watched
+      }      
+    }
+    res.status(200).send(watchList)
+  },
+  deleteFromWatchList: (req, res) => {
+    const {id} = req.params
+    for (let i = 0; i < watchList.length; i++) {
+      if (watchList[i].id === id) {
+        watchList.splice(watchList[i], 1)
+      }      
+    }
+    res.status(200).send(watchList)
   }
 };
--- server/server.js
+++ server/server.js
@@ -18,16 +18,21 @@ app.use(express.json());
 const speedrunsCtrl = require('./controllers/speedrunsCtrl')
 const watchListCtrl = require('./controllers/watchListCtrl')
 
-//endpoints
+//endpoints that pull data from speedrun.com.
 app.get(`${baseUrl}`, speedrunsCtrl.getRecentRuns)
 
 app.get(`${baseUrl}/runner/:name`, speedrunsCtrl.getRecentRunsByRunner)
 
 app.get(`${baseUrl}/game`, speedrunsCtrl.getRecentRunsByGame)
 
+//endpoints that manipulate the watch list.
 app.get(`${baseUrl}/watch-list`, watchListCtrl.getWatchList)
 
 app.post(`${baseUrl}/watch-list`, watchListCtrl.addToWatchList)
+
+app.put(`${baseUrl}/watch-list/:id`, watchListCtrl.updateWatchList)
+
+app.delete(`${baseUrl}/watch-list/:id`, watchListCtrl.deleteFromWatchList)
 
 
 
