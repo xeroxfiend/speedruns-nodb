bfeef33d25d64d0c7f185ba4a0572797a501a926
Brian Lowe <mynameisboy@gmail.com>
2019-09-08 13:09:08 -0600

    watch video almost works

--- src/components/WLRun.js
+++ src/components/WLRun.js
@@ -7,18 +7,25 @@ class WLRun extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      runsData: this.props.data
+      runsData: this.props.data,
+      clicked: false
     };
   }
 
-  imageClick(run) {
-    //play the run
+  imageClick() {
+    this.setState({clicked: !this.state.clicked});
   }
 
   remove(id) {
     axios.delete(`/api/speedrun/watch-list/${id}`).then(res => {
-      this.props.updateFn(res.data)
-    })
+      this.props.updateFn(res.data);
+    });
+  }
+
+  watched(id) {
+    axios.put(`/api/speedrun/watch-list/${id}`).then(res => {
+      this.props.updateFn(res.data);
+    });
   }
 
   render() {
@@ -27,37 +34,58 @@ class WLRun extends Component {
     const category = this.props.data.category;
 
     return (
-      <div className="run">
-        {game}
-        {videoLink.includes("twitch") ? (
-          <img
-            onClick={() => this.imageClick(this.state.runsData)}
-            className="twitch-icon"
-            width="80"
-            height="80"
-            src={twitchIco}
-            alt="twitch icon"
-          />
+      <div className="player">
+        {!this.state.clicked ? (
+          <div className="run">
+            {game}
+            {videoLink.includes("twitch") ? (
+              <img
+                onClick={() => this.imageClick()}
+                className="twitch-icon"
+                width="80"
+                height="80"
+                src={twitchIco}
+                alt="twitch icon"
+              />
+            ) : (
+              <img
+                onClick={() => this.imageClick()}
+                className="twitch-icon"
+                width="80"
+                height="80"
+                src={ytIco}
+                alt="youtube icon"
+              />
+            )}
+            {category}
+            <div className="buttons">
+              <button
+                onClick={() => this.watched(this.state.runsData.id)}
+                className="watched"
+              >
+                Watched?
+              </button>
+              <button
+                onClick={() => this.remove(this.state.runsData.id)}
+                className="remove"
+              >
+                Remove
+              </button>
+            </div>
+          </div>
         ) : (
-          <img
-            onClick={() => this.imageClick(this.state.runsData)}
-            className="twitch-icon"
-            width="80"
-            height="80"
-            src={ytIco}
-            alt="youtube icon"
-          />
+          <div className="video">
+            <button onClick={() => this.imageClick()} className="x">
+              X
+            </button>
+            <video
+              src={videoLink}
+              controls
+              height="360px"
+              width="640px"
+            ></video>
+          </div>
         )}
-        {category}
-        <div className="buttons">
-          <button className="watched">Watched?</button>
-          <button
-            onClick={() => this.remove(this.state.runsData.id)}
-            className="remove"
-          >
-            Remove
-          </button>
-        </div>
       </div>
     );
   }
